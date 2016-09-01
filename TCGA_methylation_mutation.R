3acf5baaf18729745791979989eb4369123d9618
summerliehu <14111510017@fudan.edu.cn>
2016-09-01 20:24:45 +0800

    heatmap

--- TCGA_methylation_mutation.R
+++ TCGA_methylation_mutation.R
@@ -1,10 +1,8 @@
 # read max-stddev data
 #首先需要用Excel把下下来的数据打开，删除第二行，然后把数据存成.csv的格式，在R中读取csv
-methylation <- read.csv('/Users/xiayukun/Desktop/GBM.meth.by_max_stddev.data.csv')
-#将数据框变形
-library(reshape2)
-methylation_m <- melt(methylation)
-#根据两个变量的名称作图
-library(ggplot2)
-ggplot(data = methylation_m, aes(x=variable, y=Hybridization.REF, fill=value)) + 
-    geom_tile()
+setwd('/Users/xiayukun/Desktop/')
+methylation <- read.csv('GBM.meth.by_max_stddev.data.csv')
+data=na.omit(methylation)
+
+library('gplots')
+heatmap.2(as.matrix(methylation[,-1]), col=redgreen)
